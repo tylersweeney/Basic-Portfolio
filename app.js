b3552d039fc9bb15cafced8eb1b6b3466c5c4191
Tyler <tylersweeney@gmail.com>
2018-03-20 19:32:08 -0700

    convert to ejs and app.js build

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+//set up view engine
+app.set('view engine', 'ejs');
+
+
+
+
+//connect to mongodb
+// mongoose.connect(keys.mongodb.dbURI, () => {
+//     console.log('connected to mongodb');
+// })
+
+//set up routes
+// app.use('/auth', authRoutes);
+// app.use('/profile', profileRoutes);
+
+//create home route
+app.get('/', (req, res) =>{
+   res.render('home', { user: req.user}); 
+});
+
+//create portfolia route
+app.get('/portfolio', (req, res) =>{
+    res.render('portfolio', { user: req.user}); 
+ });
+
+ //create contact route
+ app.get('/contact', (req,res)=>{
+     res.render('contact', {user: req.user});
+ })
+
+ //create about route
+ app.get('/about', (req,res)=>{
+     res.render('about', {user: req.user});
+ })
+
+app.listen(3000, () => {
+    console.log('app now listening for requests on port 3000');
+});
