e34f832042a12127ac000151f5e049a0b7d25617
Tyler <tylersweeney@gmail.com>
2018-03-20 21:18:22 -0700

    merge conflict

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-<<<<<<< HEAD
-const PORT = process.env.PORT || 8080;
-const path = require("path");
-=======
 const PORT = process.env.PORT || 3000;
 const ejs = require('ejs');
 const path = require('path');
 
->>>>>>> detached
 
 //set up view engine
 app.set('views', path.join(__dirname, 'app/views'))
@@ -28,13 +23,8 @@ app.set('view engine', 'ejs');
 // app.use('/profile', profileRoutes);
 
 //create home route
-<<<<<<< HEAD
-app.get("/", function(req,res) {
-    res.sendFile(path.join(__dirname, "./views/home"));
-=======
 app.get('/', (req, res) =>{
    res.render('home',{ user: req.user}); 
->>>>>>> detached
 });
 // app.get('/', (req, res) =>{
 //    res.render('home', { user: req.user}); 
