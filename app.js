3c788f8a209c8e10efe0c3143939d16d1cdd7cd5
Tyler Sweeney <tylersweeney@gmail.com>
2018-08-12 14:29:17 -0700

    fixed routing issue, href included file extention which caused a 404

--- app.js
+++ app.js
@@ -4,28 +4,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const ejs = require('ejs');
 const path = require('path');
+const keys = require('./config/keys');
 
-app.set('views', path.join(__dirname, 'app/views'))
-app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
-
-app.use('/images', express.static(__dirname + '/images'));
-app.use('/css', express.static(__dirname + '/css'));
 //set up view engine
-
-
-
-
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs');
 
 // connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, () => {
     console.log('connected to mongodb');
 })
-
-//set up routes
-// app.use('/auth', authRoutes);
-// app.use('/profile', profileRoutes);
 
 //create home route
 app.get('/', (req, res) =>{
@@ -51,6 +40,8 @@ app.get('/portfolio', (req, res) =>{
  app.get('/about', (req,res)=>{
      res.render('about');
  })
+
+ app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log('app now listening for requests on port:' + PORT);
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongodb: {
+        dbURI: 'mongodb://tylersweeney:4rty2wo@ds119662.mlab.com:19662/portfolio'
+    }
+}
